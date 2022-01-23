82c219aeebcdb933392950d6c4c7c957971436a0
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-23 22:23:37 +0545

    secured twilio

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
 account_sid='AC0ad61f1156fa09357de717c30824cc7d'
-auth_token='e1987d957349e19fdadf25c674d0c4f3'
+auth_token='8401b3d87820e000d81e83153c258da6'
 twilio_number='+15075225773'
 target_number='+9779861582375'
