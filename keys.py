ffda2490e778cb530778af1e668a893c001ed3f9
Anuragh Timalsina <anuraghtimalsina@gmail.com>
2022-01-25 10:52:43 +0545

    Update keys.py

--- keys.py
+++ keys.py
@@ -1,4 +1,4 @@
-account_sid='AC0ad61f1156fa09357de717c30824cc7d'
-auth_token='8401b3d87820e000d81e83153c258da6'
-twilio_number='+15075225773'
-target_number='+9779861582375'
+account_sid='<twilio_account_sid>'
+auth_token='<auth_token>'
+twilio_number='<twilio_number>'
+target_number='<target_number>'
